232c0be7eb05e505b6b96e938929a05780c1e274
Muhammet AYAN <muhammet.ayan@hotmail.com>
2019-04-09 10:13:52 +0300

    Kararlı yapı oluştu global mesaj ve userslist tmm

--- public/chat.js
+++ public/chat.js
@@ -2,14 +2,38 @@ var app = angular.module('myApp', []);
 app.controller('chat', function($scope, $http) {
     $scope.socket = io.connect();
     $scope.username = "";
-    $scope.globalMessages = []; //Global mesajlar
-    $scope.roomsMessages = {}; //Oda mesajları
-    //{}.degiskenadi = "abc"; delete {"degiskenadi": "abc"};
-    $scope.usersMessage = {};
 
-    $scope.rooms = []; //Açık odalar
-    $scope.users = []; //Açık kullanıcılar
+    $scope.writemessage = function(message, source, nowtext){
+        if(message != ""){
+            var classvalue = ""
+            switch (source) {
+                case "Siz":
+                    classvalue = "alert-success text-right";
+                    break;
+                case "":
+                    classvalue = "alert-secondary text-center";
+                    break;
+                default:
+                    classvalue = "alert-warning";
+                    break;
+            }
+            html = '<div class="' + classvalue + '">' +
+            '<div class="badge badge-dark">' + source + '</div>' +
+            '<div class="badge badge-secondary">'+ nowtext +'</div>'+
+            '<p class="mb-1">' + message + '</p>'+
+            '</div>';
+            $('#globalpanel').append(html);
+        }
+    }
 
+    $scope.writeuser = function(id, username){
+        html= '<div class="list-group-item" id="' + id + '">' + username +'</div>'
+        $('#userspanel').append(html);
+    }
+
+    $scope.deleteuser = function(id){
+        $('#' + id).remove();
+    }
 
     $('#myModal').on('click', function () {
         setTimeout(function (){
@@ -20,43 +44,47 @@ app.controller('chat', function($scope, $http) {
 
     $scope.login = function () {
         if ($scope.username != ""){
-            $('#myModal').modal('hide');
-            $('#input').focus();
+            $scope.loginsubmit();
         }else{
             alert("Lütfen kullanıcı adı giriniz.");
         }
-    }
-
-    app.directive('ngEnter', function () {
-        return function (scope, element, attrs) {
-            element.bind("keydown keypress", function (event) {
-                if (event.which === 13) {
-                    scope.$apply(function () {
-                        scope.$eval(attrs.ngEnter);
-                    });
-
-                    event.preventDefault();
-                }
-            });
-        };
-    });
-    
+    }    
     
     //Kullanıcıları getir
-    $scope.socket.emit('reqgetusers', function(data){
-        $scope.users = data.split("|")
-        $scope.$apply();
-    }); 
+    $scope.userlist = function () {    
+        $scope.socket.emit('reqgetusers', function(data){
+            $('#userspanel').empty();
+            $('#globalpanel').empty();
+            var tokens = data.split("|")
+            for(var i in tokens){
+                if (tokens[i] != undefined && tokens[i] != ""){
+                var t  = tokens[i].split(":");
+                $scope.writeuser(t[0]/*id*/, t[1]/*username*/)
+                }
+            }
+        }); 
+    }
+    $scope.loginsubmit = function () {
+        $scope.socket.emit('reqlogin', $scope.username, function (status, message) {
+            if (status){
+                $('#myModal').modal('hide');
+                $('#input').focus();
+                $scope.userlist();
+            }else{
+                alert(message)
+            }
+        })
+    }
     
     $scope.herkeseMesajGonder = function (message) {
         $scope.socket.emit('reqglobal', message, $scope.username, function(data){
              var now = new Date();
              var nowtext = now.getHours() + ":" + now.getMinutes()
-             $scope.globalMessages.push({'index':$scope.globalMessages.length, 'source':'Siz', 'message':data, 'date':nowtext})
+             $scope.writemessage(message, 'Siz', nowtext)
         })
         var now = new Date();
         var nowtext = now.getHours() + ":" + now.getMinutes()
-        $scope.globalMessages.push({'index':$scope.globalMessages.length, 'source':'Siz', 'message':message, 'date':nowtext})
+        $scope.writemessage(message, 'Siz', nowtext)
         $scope.text = ""
     }
 
@@ -70,8 +98,18 @@ app.controller('chat', function($scope, $http) {
 
     // Arkayüzden tetiklenen mesajları yakalamak için
     $scope.socket.on('resglobal', function(data, source, date) {
-        $scope.globalMessages.push({'index':$scope.globalMessages.length, 'source': source, 'message':data, 'date': date})
-        $scope.$apply();
+        $scope.writemessage(data, source, date)
     })
 
+    $scope.socket.on('resuserlist', function(status, id, username) {
+        if (status == "login"){
+            $scope.writeuser(id, username);
+            $scope.writemessage(username + " adlı kullanıcı giriş yaptı.", '', '');
+        }
+        if (status == "logout"){
+            $scope.writemessage(username + " adlı kullanıcı ayrıldı.", '', '');
+            $scope.deleteuser(id);
+        }
+            
+    })
 });
--- server.js
+++ server.js
@@ -16,46 +16,83 @@ app.get(/^\/public\/.*$/, function(req, res){
     res.sendFile(req.url, {root: rootPath});
 });
 
+app.get("/", function (req, res) {
+    res.redirect("/chat.html")
+})
+
 //html requests
 app.get(/.*\.html$/, function(req, res){
      res.sendFile(req.url, {root: viewsPath});
 });
 
-/*
-app.get("", function (req, res) {
-    
-})*/
+
+function selectElement(array, key, data) {
+    for (let i = 0; i < array.length; i++) {
+        if(array[i][key] == data){
+            return i;
+        }
+    }
+    return -1;
+}
+function removeElement(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] == element){
+            delete array.splice(i, 1);
+        }
+    }
+    return array;
+}
+
 
 var users = [];
 
 io.on('connection', client => {
-    console.log(client.id)
-    users.push(client.id)
-  
     client.on('disconnect', function () {
-        for(var i =0; i < users.length; i++){
-            if(users[i] == client.id){
-                users.splice(i, 1);
-                break;
-            }
+        var i = selectElement(users, "id", client.id)
+        
+        if(i != -1){
+            username = users[i]['username']
+            //console.log(users[i] + " " + i)
+            client.broadcast.emit("resuserlist", "logout", client.id, username)
+            users.splice(i, 1);
         }
     });
-    
+    /*
     client.join('room', () => {
         let rooms = Object.keys(client.rooms)
         console.log("[" + rooms[0] + "]:'" +  rooms[1] + "' odasına bağlandı.")
     })
-    
+    */
     client.on('reqgetusers',function(callback){
         var temp = ""
         for(var i in users){
-            temp+=users[i]+"|"
+            if(users[i]['id'] != client.id)
+                temp+=users[i]["id"] + ":" + users[i]["username"]+"|"
         }
         temp = temp.substr(0, temp.length - 1);
-        console.log("kullanıcılar çalıştı:" + temp)
         callback(temp)
     })
     
+    client.on('reqlogin', function(username, callback){
+        var control = false;
+        for(var i in users){
+            if(users[i]['username'] == username){
+            control = true;
+            break;
+            }
+        }
+        
+        if (username == "")
+            callback(false, "Kullanıcı adı boş girmeyiniz.")
+        else if (control)
+            callback(false, "Böyle bir kullanıcı zaten var!")
+        else{
+            users.push({'id': client.id, 'username': username})
+            callback(true, "")
+            var i = selectElement(users, "id", client.id)
+            client.broadcast.emit("resuserlist", "login", client.id, users[i]['username'])
+        }
+    })
 
     client.on('reqglobal', function(data, source, callback){
         
@@ -73,9 +110,6 @@ io.on('connection', client => {
     })
     
 })
-
-
-
 
 
 server.listen(3000)
